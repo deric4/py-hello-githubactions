8e62738d04be1b4b1fb2ca1d8d869d5779a0bd44
Deric Miguel <deric.miguel@protonmail.com>
2020-05-04 00:36:41 -0700

    use correct path module import

--- action.py
+++ action.py
@@ -1,8 +1,8 @@
 from githubactions.core import command as cmd
-from githubactions.github import context
+from githubactions.github.context import context
 import os
 
 
 i = cmd.get_input('dummy', {})
-cmd.set_output('o', type(context))
+cmd.set_output('o', context.json())
 
